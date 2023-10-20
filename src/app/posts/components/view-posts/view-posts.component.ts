529de04e5f364537429063cb7fc259a484dedeab
dabuk58 <jakub.deska@simpleformat.pl>
2023-10-20 12:13:14 +0200

    edited way of data loading into app

--- src/app/posts/components/view-posts/view-posts.component.ts
+++ src/app/posts/components/view-posts/view-posts.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostsService } from '../../services/posts.service';
+import { Post } from '../../models/post.model';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-posts',
   templateUrl: './view-posts.component.html',
   styleUrls: ['./view-posts.component.scss']
 })
-export class ViewPostsComponent implements OnInit{
-
+export class ViewPostsComponent implements OnInit, OnDestroy{
+  // private posts = new Subject<Post[]>();
+  private posts: Post[] = [];
+  private subscription!: Subscription;
 
   constructor(private postsService: PostsService){}
 
   ngOnInit(): void {
-    this.postsService.fetchPosts();
+    this.subscription = this.postsService.fetchPosts().subscribe(post => {
+      this.posts = post;
+    });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -5,25 +5,20 @@ import { Subject, map } from 'rxjs';
 
 @Injectable()
 export class PostsService {
-  private posts = new Subject<Post[]>();
 
   constructor(private http: HttpClient) { }
 
   fetchPosts(){
     return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
       .pipe(map(post => {
-        console.log(post);
         return post.map(
           postData => new Post(
             postData.userId,
             postData.id,
             postData.title,
             postData.body
-          )
-        );
-      })).subscribe(post => {
-        this.posts.next(post);
-      });
+          ));
+      }));
   }
 
 }
