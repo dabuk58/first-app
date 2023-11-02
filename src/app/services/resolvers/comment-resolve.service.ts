284dbd8c96c839345001808336ba3456b6d0a969
dabuk58 <jakub.deska@simpleformat.pl>
2023-11-02 12:57:14 +0100

    changes types of resolved data in resolvers

--- src/app/services/resolvers/comment-resolve.service.ts
+++ src/app/services/resolvers/comment-resolve.service.ts
@@ -2,14 +2,16 @@ import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular
 import { CommentsService } from "../comments.service";
 import { inject } from "@angular/core";
 import { map } from "rxjs";
+import { CommentData } from "src/app/comments/models/comment.model";
 
 
-export const commentResolver: ResolveFn<any> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, commentService: CommentsService = inject(CommentsService)) => {
+export const commentResolver: ResolveFn<CommentData[]> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, commentService: CommentsService = inject(CommentsService)) => {
     const postId = +route.params['postId'];
 
     return commentService.fetchComments(postId)
         .pipe(map(
             comments => comments.slice(0,3)
+
             )
         );
 }
--- src/app/services/resolvers/posts-resolve.service.ts
+++ src/app/services/resolvers/posts-resolve.service.ts
@@ -1,9 +1,9 @@
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import { inject } from "@angular/core";
-import { map } from "rxjs";
 import { PostsService } from "../posts.service";
+import { Post } from "src/app/posts/models/post.model";
 
 
-export const postsResolver: ResolveFn<any> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, postsService: PostsService = inject(PostsService)) => {
+export const postsResolver: ResolveFn<Post[]> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot, postsService: PostsService = inject(PostsService)) => {
     return postsService.fetchPosts();
 }
